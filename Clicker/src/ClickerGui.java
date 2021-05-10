fdab304a73be1f5fbcabc7e4786f84362afa9891
alexander.holmstro3 <alle020916@gmail.com>
2021-05-10 11:07:17 +0200

    Fixat med Gui

--- Clicker/src/ClickerGui.java
+++ Clicker/src/ClickerGui.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 
 public class ClickerGui {
     private JPanel Jpanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JTextArea textArea1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("ClickerGui");
