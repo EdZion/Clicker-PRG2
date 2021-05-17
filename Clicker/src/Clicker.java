8fa1df63af0affc0a655622caba4b862f7d5864c
alexander.holmstro3 <alle020916@gmail.com>
2021-05-17 11:10:39 +0200

    Slut av lektionen

--- Clicker/src/Clicker.java
+++ Clicker/src/Clicker.java
@@ -1,4 +1,5 @@
 public class Clicker {
+    int counter;
 
 }
 
---
+++ Clicker/src/view.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+
+public class view {
+    private JPanel Jpanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton clickerButton;
+    private JTextArea textArea1;
+
+    public void setJpanel(JPanel jpanel) {
+        Jpanel = jpanel;
+    }
+
+    public void setButton1(JButton button1) {
+        this.button1 = button1;
+    }
+
+    public void setButton2(JButton button2) {
+        this.button2 = button2;
+    }
+
+    public void setButton3(JButton button3) {
+        this.button3 = button3;
+    }
+
+    public void setButton4(JButton button4) {
+        this.button4 = button4;
+    }
+
+    public void setButton5(JButton button5) {
+        this.button5 = button5;
+    }
+
+    public void setButton6(JButton button6) {
+        this.button6 = button6;
+    }
+
+    public void setClickerButton(JButton clickerButton) {
+        this.clickerButton = clickerButton;
+    }
+
+    public void setTextArea1(JTextArea textArea1) {
+        this.textArea1 = textArea1;
+    }
+
+    public JPanel getJpanel() {
+        return Jpanel;
+    }
+
+    public JButton getButton1() {
+        return button1;
+    }
+
+    public JButton getButton2() {
+        return button2;
+    }
+
+    public JButton getButton3() {
+        return button3;
+    }
+
+    public JButton getButton4() {
+        return button4;
+    }
+
+    public JButton getButton5() {
+        return button5;
+    }
+
+    public JButton getButton6() {
+        return button6;
+    }
+
+    public JButton getClickerButton() {
+        return clickerButton;
+    }
+
+    public JTextArea getTextArea1() {
+        return textArea1;
+    }
+}
